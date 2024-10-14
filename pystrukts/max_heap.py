8e1f3bccee7833465fc634c9bd51177cdc6c70f9
rubenperezm <rubenpermerc@gmail.com>
2024-10-14 11:14:25 +0200

    docs: change typing
    Change from any function to typing.Any

--- pystrukts/max_heap.py
+++ pystrukts/max_heap.py
@@ -6,6 +6,7 @@ It provides methods for inserting, extracting, and peeking at the maximum value 
 '''
 
 from dataclasses import field, dataclass
+from typing import Any
 import heapq
 
 @dataclass
@@ -19,7 +20,7 @@ class MaxHeap:
     Methods:
         `__post_init__()`: Creates the heap from the data.
         `__str__()`: Return the string representation of the heap.
-        `push(value: any)`: Insert a value in the heap.
+        `push(value: Any)`: Insert a value in the heap.
         `pop()`: Extract the maximum value from the heap.
         `peek()`: Peek the maximum value in the heap.
     '''
@@ -36,12 +37,12 @@ class MaxHeap:
     def __str__(self):
         return f"MaxHeap({[-val for val in self.data]})"
 
-    def push(self, value: any):
+    def push(self, value: Any):
         '''
         Insert a value in the heap.
 
         Args:
-            value (any): The value to be inserted.
+            value (Any): The value to be inserted.
         
         Raises:
             TypeError: If the value is not comparable.
@@ -54,7 +55,7 @@ class MaxHeap:
         Extract the maximum value from the heap.
 
         Returns:
-            out (any): The maximum value in the heap.
+            out (Any): The maximum value in the heap.
 
         Raises:
             IndexError: If the heap is empty.
@@ -66,6 +67,6 @@ class MaxHeap:
         Peek the maximum value in the heap.
 
         Returns:
-            out (any): The maximum value in the heap or `None` if the heap is empty.
+            out (Any): The maximum value in the heap or `None` if the heap is empty.
         '''
         return -self.data[0]
