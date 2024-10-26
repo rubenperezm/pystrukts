57ffc716fbfa9f3e4103e493388cf84f4a09032e
rubenperezm <rubenpermerc@gmail.com>
2024-10-26 11:37:26 +0200

    feat: fenwick tree
    Add Fenwick tree implmentation and tests

--- pystrukts/tree/__init__.py
+++ pystrukts/tree/__init__.py
@@ -4,3 +4,4 @@ from .trie import Trie, TrieNode
 from .generic_tree import GenericTree
 from .binary_tree import BinaryTree
 from .binary_search_tree import BinarySearchTree
+from .fenwick_tree import FenwickTree, RUPQ, RURQ
